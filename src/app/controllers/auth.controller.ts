dcadeb56605f54d316e0bc279951b232a3906861
Jose Castro <drakonc@gmail.com>
2019-10-10 16:33:24 -0500

    Finalizando

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -30,8 +30,6 @@ export const singin = async (req: Request, res: Response) => {
 	const correctPassword: boolean = await user.validatePassword(password);
 	if(!correctPassword) return res.status(400).json('Invalid Password');
 
-	console.log('antes del')
-
 	//token
 	const token: string = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET || 'v6qOLR6R62GSKo',{
 		expiresIn: 60*60*24
@@ -40,6 +38,8 @@ export const singin = async (req: Request, res: Response) => {
 	res.header('auth-token', token).json(user);
 };
 
-export const profile = (req: Request, res: Response) => {
-	res.send('profile')
+export const profile = async (req: Request, res: Response) => {
+	const user = await User.findById(req.userId,{ password: 0});
+	if(!user) return res.status(400).json('User no Found');
+	res.status(200).json(user);
 };
